8f74f110f10f56c65ad0bf3853f1a30d0716528b
SkippyTheIdiot <SkippyTheIdiot@outlook.com>
2019-04-10 00:45:17 +1000

    Updated flash.bat

--- Projects/Fragmented-LED-Display/flash.bat
+++ Projects/Fragmented-LED-Display/flash.bat
@@ -1,10 +1,12 @@
 particle compile xenon --saveTo xenon.bin
-particle flash Xenon_Walrus xenon.bin
-particle flash Xenon_Dolphin xenon.bin
-particle flash Xenon_Pidgeon xenon.bin
-particle flash Xenon_Gerbil xenon.bin
 
-SLEEP 10
+start /min cmd.exe /c particle flash Xenon_Walrus xenon.bin
+start /min cmd.exe /c particle flash Xenon_Dolphin xenon.bin
+start /min cmd.exe /c particle flash Xenon_Pidgeon xenon.bin
+start /min cmd.exe /c particle flash Xenon_Gerbil xenon.bin
 
 particle compile argon --saveTo argon.bin
+
+timeout 30 /nobreak
+
 particle flash Argon_Deer argon.bin
