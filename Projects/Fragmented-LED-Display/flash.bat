255e9ef24c1d2b0d109b80e3eebf6a2e8cb3c330
Skippy <SkippyTheIdiot@outlook.com>
2019-04-10 00:53:17 +1000

    Update flash.bat

--- Projects/Fragmented-LED-Display/flash.bat
+++ Projects/Fragmented-LED-Display/flash.bat
@@ -1,12 +1,21 @@
+:: A quick way to update the firmware across all of the devices
+
 particle compile xenon --saveTo xenon.bin
 
+:: Follow the following structure, add each Xenon
 start /min cmd.exe /c particle flash Xenon_Walrus xenon.bin
 start /min cmd.exe /c particle flash Xenon_Dolphin xenon.bin
 start /min cmd.exe /c particle flash Xenon_Pidgeon xenon.bin
 start /min cmd.exe /c particle flash Xenon_Gerbil xenon.bin
 
 particle compile argon --saveTo argon.bin
+:: particle compile boron --saveTo argon.bin
 
+:: Add delay to allow slave devices to flash or timeout
 timeout 30 /nobreak
 
+:: Replace this with the Argon gateway
 particle flash Argon_Deer argon.bin
+
+:: Replace this with the Boron gateway
+:: particle flash Boron boron.bin
